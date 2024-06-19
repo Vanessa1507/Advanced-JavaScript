4c0bf7f138104e66f13c828c51604418561711b6
Vanessa1507 <lauritasuarezg@gmail.com>
2024-06-18 22:31:25 -0500

    types & equality

---
+++ types_equality/diference_==_===.js
@@ -0,0 +1,9 @@
+NaN == 1; // false
+NaN == false;  // false
+NaN == NaN; // false
+
+isNaN(NaN); //true
+
+isNaN(1); //false
+isNaN("1"); //false
+isNaN("A"); //true
---
+++ types_equality/nan_property.js
@@ -0,0 +1,15 @@
+0 === 0; // true
+0 !== 1; // true
+
+0 == 0; // true
+0 != 1; // true
+
+'' == '0'; //false
+
+0 == ''; //true
+0 == '0'; //true
+
+0 === ''; //false
+0 === '0'; //false
+
+false == 'false'
